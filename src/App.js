17c700bf2e1b06a0ace9815be747269fbc1db547
Michal Czyzowicz <czyzowicz.m@gmail.com>
2021-01-17 00:16:05 +0100

    add book component

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Book from "./Book";
+
+const App = () => {
+  return (
+    <div>
+      <h1>React Bookstore</h1>
+      <Book
+        title="Straz! Straz!"
+        author="Terry Pratchett"
+        publicationDate="1989"
+      />
+      <Book
+        title="Dwie wieze"
+        author="J. R. R. Tolkien"
+        publicationDate="1954"
+      />
+      <Book
+        title="Komnata Tajemnic"
+        author="J. K. Rowling"
+        publicationDate="1998"
+      />
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+export default App;
